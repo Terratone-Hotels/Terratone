71e93af21b0903691df5bae5f504db30f8eefc9f
Terratone-Hotels <marketing@terratonehotels.com>
2025-09-22 13:06:31 +0530

    first commit

---
+++ src/slices/Hero/index.js
@@ -0,0 +1,19 @@
+import { PrismicRichText } from "@prismicio/react";
+
+/**
+ * @typedef {import("@prismicio/client").Content.HeroSlice} HeroSlice
+ * @typedef {import("@prismicio/react").SliceComponentProps<HeroSlice>} HeroProps
+ * @type {import("react").FC<HeroProps>}
+ */
+const Hero = ({ slice }) => {
+  return (
+    <section
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      <PrismicRichText field={slice.primary.heading} />
+    </section>
+  );
+};
+
+export default Hero;
