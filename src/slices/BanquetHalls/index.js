cdb51e57aef61c9c646e43afcbd09f09bcf08f37
Terratone-Hotels <marketing@terratonehotels.com>
2025-10-03 10:50:20 +0530

    fine tuning 2(except 2 slices)

--- src/slices/BanquetHalls/index.js
+++ src/slices/BanquetHalls/index.js
@@ -2,7 +2,7 @@
 
 import Bounded from "@/components/Bounded";
 import Button from "@/components/Button";
-import {PrismicNextLink,  PrismicNextImage } from "@prismicio/next";
+import { PrismicNextLink, PrismicNextImage } from "@prismicio/next";
 import { PrismicRichText } from "@prismicio/react";
 
 /**
@@ -12,38 +12,46 @@ import { PrismicRichText } from "@prismicio/react";
  */
 const BanquetHalls = ({ slice }) => {
   return (
-    <Bounded full
-      className={"relative h-auto "}
+    <Bounded
+      className={"relative h-auto max-w-[90rem] mx-auto mt-20  "}
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      
-
       <div className="flex flex-col gap-5 relative h-[40.6875rem] md:h-[46.5rem] ">
-        <PrismicNextImage field={slice.primary.banquet_image} className="w-full h-full object-cover" />
+        <PrismicNextImage
+          field={slice.primary.banquet_image}
+          className="w-full h-full object-cover"
+        />
         <div className="absolute top-5 left-4 md:top-26 md:left-35 text-white ">
-          <PrismicRichText field={slice.primary.heading} components={{   heading1: ({ children }) => (
+          <PrismicRichText
+            field={slice.primary.heading}
+            components={{
+              heading1: ({ children }) => (
                 <h2 className="text-[1.75rem] md:text-[2.625rem] font-serif font-medium leading-7 md:leading-12   ">
                   {children}
                 </h2>
-              )}} />
-          <div className=" my-4 w-[95%] md:w-[30%] md:my-6">
-            <PrismicRichText field={slice.primary.description} 
-            components={{
-                            paragraph: ({ children }) => (
-                <p className="text-sm md:text-lg font-bar text-white tracking-wide leading-tight">
-                  {children}
-                </p>
-              )
+              ),
             }}
+          />
+          <div className=" my-4 w-[95%] md:w-[30%] md:my-6">
+            <PrismicRichText
+              field={slice.primary.description}
+              components={{
+                paragraph: ({ children }) => (
+                  <p className="text-sm md:text-lg font-bar text-white tracking-wide leading-tight">
+                    {children}
+                  </p>
+                ),
+              }}
             />
           </div>
-          <Button showArrow className={"text-xs"}><PrismicNextLink field={slice.primary.button_link}>{slice.primary.button_text}</PrismicNextLink></Button>
-          
+          <Button showArrow className={"text-xs"}>
+            <PrismicNextLink field={slice.primary.button_link}>
+              {slice.primary.button_text}
+            </PrismicNextLink>
+          </Button>
         </div>
-
       </div>
-      
     </Bounded>
   );
 };
