e3c05f13880e285c723fa6313e52cdea50eac956
Terratone-Hotels <marketing@terratonehotels.com>
2025-10-03 09:27:10 +0530

    meetinghalls tuned

--- src/slices/MeetingHalls/index.js
+++ src/slices/MeetingHalls/index.js
@@ -4,6 +4,7 @@ import { PrismicNextImage } from "@prismicio/next";
 import { PrismicRichText } from "@prismicio/react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
+import Bounded from "@/components/Bounded";
 
 /**
  * @typedef {import("@prismicio/client").Content.MeetingHallsSlice} MeetingHallsSlice
@@ -15,102 +16,122 @@ const MeetingHalls = ({ slice }) => {
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
-      
+      className=" mt-20 md:mt-44"
     >
+      <div className="relative h-12  w-40 mx-auto mb-8">
+        {/* left low dot */}
+        <span className="absolute left-0 top-6 w-2 h-2 rounded-full bg-[#977161]" />
+
+        {/* left raised dot */}
+        <span className="absolute left-8 top-0 w-2 h-2 rounded-full bg-[#996353]" />
+
+        {/* center low dot */}
+        <span className="absolute left-1/2 -translate-x-1/2 top-10 w-2 h-2 rounded-full bg-[#5B554C]" />
+
+        {/* right raised dot */}
+        <span className="absolute right-12 top-2 w-2 h-2 rounded-full bg-[#857161]" />
+
+        {/* right low dot */}
+        <span className="absolute right-2 top-6 w-2 h-2 rounded-full bg-[#AF9381]" />
+      </div>
+
       {/* Heading + description */}
-      <div className="max-w-3xl mx-auto text-center mb-12 px-4 ">
-        <PrismicRichText
-          field={slice.primary.heading}
-          components={{
-            heading1: ({ children }) => (
-              <h2 className="text-3xl  md:text-4xl font-serif font-semibold mb-4">
-                {children}
-              </h2>
-            ),
-            heading2: ({ children }) => (
-              <h2 className="text-3xl md:text-4xl  mb-4">
-                {children}
-              </h2>
-            ),
-          }}
-        />
+      <Bounded
+        data-slice-type={slice.slice_type}
+        data-slice-variation={slice.variation}
+        full
+      >
+        <div className="max-w-3xl mx-auto text-center mb-12 px-4  mt-8 md:mt-12">
+          <PrismicRichText
+            field={slice.primary.heading}
+            components={{
+              heading1: ({ children }) => (
+                <h2 className="text-3xl  md:text-5xl font-serif font-medium mb-4">
+                  {children}
+                </h2>
+              ),
+              heading2: ({ children }) => (
+                <h2 className="text-3xl md:text-5xl font-serif  mb-4">
+                  {children}
+                </h2>
+              ),
+            }}
+          />
 
-        <PrismicRichText
-          field={slice.primary.description}
-          components={{
-            paragraph: ({ children }) => (
-              <p className="text-base font-barlow md:text-lg text-gray-700">{children}</p>
-            ),
-          }}
-        />
-        <div>
-        <Button variant="secondary"  className="mt-5 font-barlow mr-1" >ABOUT US  </Button>
-        <Button showArrow variant="secondary"/>
+          <PrismicRichText
+            field={slice.primary.description}
+            components={{
+              paragraph: ({ children }) => (
+                <p className="text-base leading-tight font-barlow md:text-[18px] ">
+                  {children}
+                </p>
+              ),
+            }}
+          />
         </div>
-      </div>
 
-      {/* Desktop Grid (hover overlay) */}
-      <div className="hidden lg:grid lg:grid-cols-3 gap-20 max-w-6xl mx-auto px-4">
-        {slice.primary.rooms.map((item, index) => (
-          <div key={index} className="group relative">
-            <div className="relative overflow-hidden">
-              <PrismicNextImage
-                field={item.image}
-                className="w-full h-64 object-cover"
-              />
-              {/* Hover button for desktop */}
-
-              {/* <button className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-300">
-                
-              </button> */}
-
-        <Button showArrow className={"absolute  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex opacity-0 group-hover:opacity-100 transition duration-300"}> 
-          VIEW ROOM
-        </Button>
-            </div>
-            <div className="mt-4">
-              <h3 className="text-[1.375rem] font-medium font-serif">
-                {item.card_title}
-              </h3>
-              <p className="text-xs font-barlow text-gray-600 mt-1">
-                {item.card_description}
-              </p>
-            </div>
-          </div>
-        ))}
-      </div>
-
-      <div className="lg:hidden px-4">
-        <Swiper spaceBetween={16} slidesPerView={1.3} grabCursor={true}>
+        {/* Desktop Grid (hover overlay) */}
+        <div className="hidden lg:flex gap-40 max-w-[85%] mx-auto ">
           {slice.primary.rooms.map((item, index) => (
-            <SwiperSlide key={index}>
-              <div className="relative">
-                <div className="relative overflow-hidden rounded-md">
-                  <PrismicNextImage
-                    field={item.image}
-                    className="w-full h-64 object-cover"
-                  />
+            <div key={index} className="group relative">
+              <div className="relative overflow-hidden ">
+                {/* Gradient overlay */}
+                <div className="absolute inset-0 bg-black/40 z-10" />
+                <PrismicNextImage
+                  field={item.image}
+                  className="w-full aspect-square  object-cover"
+                />
 
-                  <div className="absolute inset-0 flex items-center justify-center">
-                    {/* <span className="bg-white text-black px-6 py-2 text-sm font-medium shadow-md">
-                      VIEW ROOM 
-                    </span> */}
-                    <Button showArrow> VIEW ROOM </Button>
+                <Button
+                  showArrow
+                  className={
+                    "absolute z-20  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex opacity-0 group-hover:opacity-100 transition duration-300"
+                  }
+                >
+                  VIEW ROOM
+                </Button>
+              </div>
+              <div className="mt-4">
+                <h3 className="text-[1.375rem] font-medium font-serif">
+                  {item.card_title}
+                </h3>
+                <p className="text-[14px] font-barlow mt-2">
+                  {item.card_description}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div className="lg:hidden pl-4">
+          <Swiper spaceBetween={20} slidesPerView={1.2} grabCursor={true}>
+            {slice.primary.rooms.map((item, index) => (
+              <SwiperSlide key={index}>
+                <div className="relative">
+                  <div className="relative overflow-hidden ">
+                    {/* Gradient overlay */}
+                    <div className="absolute inset-0 bg-black/40 z-10" />
+                    <PrismicNextImage
+                      field={item.image}
+                      className="aspect-square object-cover"
+                    />
+
+                    <div className="absolute inset-0 z-20 flex items-center justify-center">
+                      <Button showArrow> VIEW ROOM </Button>
+                    </div>
+                  </div>
+                  <div className="mt-4">
+                    <h3 className="text-lg font-semibold font-serif ">
+                      {item.card_title}
+                    </h3>
+                    <p className="text-sm  ">{item.card_description}</p>
                   </div>
                 </div>
-                <div className="mt-4">
-                  <h3 className="text-lg font-semibold font-serif ">
-                    {item.card_title}
-                  </h3>
-                  <p className="text-sm text-gray-600  mt-1">
-                    {item.card_description}
-                  </p>
-                </div>
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      </Bounded>
     </section>
   );
 };
--- src/slices/OneWithSideWords/index.js
+++ src/slices/OneWithSideWords/index.js
@@ -1,4 +1,5 @@
 import Bounded from "@/components/Bounded";
+import Button from "@/components/Button";
 import VideoComponent from "@/components/VideoComponent";
 import { PrismicRichText } from "@prismicio/react";
 
@@ -12,7 +13,7 @@ const OneWithSideWords = ({ slice }) => {
     <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
-      className="flex flex-col items-center justify-center text-center py-12"
+      className="flex flex-col items-center justify-center text-center py-12 mt-8 md:mt-44"
     >
       {/* Top heading */}
       <div className="text-[1.75rem] md:text-[2.625rem] font-serif font-medium">
@@ -20,19 +21,20 @@ const OneWithSideWords = ({ slice }) => {
       </div>
 
       {/* Video + side words wrapper */}
-      <div className="flex items-center justify-center w-full mt-8">
+      <div className="flex items-center justify-center w-full ">
         {/* Left word */}
-        <div className="relative w-[20%] md:w-auto flex items-center justify-center">
+        <div className="relative w-[20%] lg:w-auto flex items-center justify-center ">
           <div
-            className="text-[1.75rem] md:text-[2.625rem] font-serif font-medium
-                          md:rotate-0 -rotate-90 md:text-right md:pr-10"
+            className="text-[1.75rem] lg:text-[2.625rem] font-serif font-medium
+                          lg:rotate-0 -rotate-90 lg:text-right lg:pr-10"
           >
             <PrismicRichText field={slice.primary.left_word} />
           </div>
         </div>
 
         {/* Video in the center */}
-        <div className="w-[60%]">
+        {/* Video in the center */}
+        <div className="w-full md:w-[60%] aspect-[4/3] md:aspect-[16/9]">
           <VideoComponent
             srcMp4={slice.primary.video}
             className="w-full h-full object-cover"
@@ -40,18 +42,24 @@ const OneWithSideWords = ({ slice }) => {
         </div>
 
         {/* Right word */}
-        <div className="relative w-[20%] md:w-auto flex items-center justify-center">
+        <div className="relative w-[20%] lg:w-auto flex items-center justify-center">
           <div
-            className="text-[1.75rem] md:text-[2.625rem] font-serif font-medium
-                          md:rotate-0 rotate-90 md:text-left md:pl-10"
+            className="text-[1.75rem] lg:text-[2.625rem] font-serif font-medium
+                          lg:rotate-0 rotate-90 md:text-left lg:pl-10"
           >
             <PrismicRichText field={slice.primary.right_word} />
           </div>
         </div>
       </div>
 
-      <div className="mt-8 font-barlow text-sm md:text-lg text-black text-center">
+      <div className="mt-8 leading-tight font-barlow text-sm md:text-[18px]  text-black text-center">
         <PrismicRichText field={slice.primary.description} />
+      </div>
+      <div className="mt-6 flex justify-center items-center gap-2  ">
+        <Button variant="secondary" className=" font-barlow">
+          ABOUT US
+        </Button>
+        <Button showArrow variant="secondary" />
       </div>
     </Bounded>
   );
